8e13024c820ca2b9e7948e300309a6279158eeb3
vahid almasi <vahid-almasi@users.noreply.github.com>
2020-04-09 22:47:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 require 'core/Bootstrap.php';
 
-$router = Router::load(__DIR__ . '/routes.php')->direct(Request::uri(), Request::method());
+Router::load(__DIR__ . '/routes.php')->direct(Request::uri(), Request::method());
